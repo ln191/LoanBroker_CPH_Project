1db81eeda8375a4fc3d8d5ade5f03876efe5a5e2
ln191 <cph-ln191@cphbusiness.dk>
2016-12-08 12:04:46 +0100

    cleanup coded

--- CreditScore/Credit.cs
+++ CreditScore/Credit.cs
@@ -12,16 +12,10 @@ namespace CreditScore
 {
     public class CreditEnricher
     {
-        //private const string HostName = "localhost";
-        //private const string UserName = "guest";
-        //private const string Password = "guest";
         private string receiveQueueName;
 
         private string sendToQueueName;
 
-        //private ConnectionFactory connectionFactory;
-        //private IConnection connection;
-        //private IModel channel;
         private RabbitConnection rabbitConn;
 
         public CreditEnricher(string receiveQueueName, string sendToQueueName)
@@ -31,37 +25,7 @@ namespace CreditScore
             this.sendToQueueName = sendToQueueName;
             rabbitConn.Channel.QueueDeclare(queue: receiveQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
             rabbitConn.Channel.QueueDeclare(queue: sendToQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-            //SetupRabbitMq();
         }
-
-        //private void SetupRabbitMq()
-        //{
-        //    connectionFactory = new ConnectionFactory
-        //    {
-        //        HostName = HostName,
-        //        UserName = UserName,
-        //        Password = Password
-        //    };
-
-        //    connection = connectionFactory.CreateConnection();
-        //    channel = connection.CreateModel();
-        //    channel.QueueDeclare(queue: receiveQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-        //    channel.QueueDeclare(queue: sendToQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-
-        //    #region basicQos Notes
-
-        //    /*
-        //     *  basicQos method with the prefetchCount = 1 setting.
-        //     *  This tells RabbitMQ not to give more than one message to a worker at a time.
-        //     *  Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one.
-        //     *  Instead, it will dispatch it to the next worker that is not still busy.
-        //     */
-
-        //    #endregion basicQos Notes
-
-        //    //so this program will take one message at the time
-        //    channel.BasicQos(0, 1, false);
-        //}
 
         public void StartReceiving()
         {
@@ -120,19 +84,5 @@ namespace CreditScore
 
             return 20;
         }
-
-        //private void Send(string message, IBasicProperties header)
-        //{
-        //    //setup header properties
-        //    var properties = header;
-        //    properties.Persistent = true;
-
-        //    //Serialize
-        //    byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
-
-        //    //Send message
-        //    channel.BasicPublish("", sendToQueueName, properties, messageBuffer);
-        //    Console.WriteLine("message: {0}, send to {1} channel", message, sendToQueueName);
-        //}
     }
 }
--- GetBanks/BankEnricher.cs
+++ GetBanks/BankEnricher.cs
@@ -12,16 +12,10 @@ namespace GetBanks
 {
     public class BankEnricher
     {
-        //private const string HostName = "localhost";
-        //private const string UserName = "guest";
-        //private const string Password = "guest"
         private string receiveQueueName;
 
         private string sendToQueueName;
 
-        //private ConnectionFactory connectionFactory;
-        //private IConnection connection;
-        //private IModel channel;
         private RabbitConnection rabbitConn;
 
         public BankEnricher(string receiveQueueName, string sendToQueueName)
@@ -31,26 +25,7 @@ namespace GetBanks
             this.sendToQueueName = sendToQueueName;
             rabbitConn.Channel.QueueDeclare(queue: receiveQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
             rabbitConn.Channel.QueueDeclare(queue: sendToQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-            //SetupRabbitMq();
         }
-
-        //private void SetupRabbitMq()
-        //{
-        //    connectionFactory = new ConnectionFactory
-        //    {
-        //        HostName = HostName,
-        //        UserName = UserName,
-        //        Password = Password
-        //    };
-
-        //    connection = connectionFactory.CreateConnection();
-        //    channel = connection.CreateModel();
-        //    channel.QueueDeclare(queue: receiveQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-        //    channel.QueueDeclare(queue: sendToQueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-        //    //so it will take one message at the time
-        //    //so it will take one message at the time
-        //    channel.BasicQos(0, 1, false);
-        //}
 
         public void StartReceiving()
         {
@@ -89,19 +64,5 @@ namespace GetBanks
             tempBanks.Add(danskeBank);
             return tempBanks;
         }
-
-        //private void Send(string message, IBasicProperties header)
-        //{
-        //    //setup header properties
-        //    var properties = header;
-        //    properties.Persistent = true;
-
-        //    //Serialize
-        //    byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
-
-        //    //Send message
-        //    channel.BasicPublish("", sendToQueueName, properties, messageBuffer);
-        //    Console.WriteLine("message: {0}, send to {1} channel", message, sendToQueueName);
-        //}
     }
 }
